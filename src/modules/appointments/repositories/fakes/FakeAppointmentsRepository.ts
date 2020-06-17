e854982a0490cbc3b396e6868c82b4b105a68a18
Lucas Gregory <lgregory12@hotmail.com>
2020-06-17 18:34:44 -0300

    Create appointment unit test.

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,37 @@
+import { uuid } from 'uuidv4';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+import Appointment from '../../infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, {
+      id: uuid(),
+      date,
+      provider_id,
+    });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const foundAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return foundAppointment;
+  }
+}
+
+export default AppointmentsRepository;
