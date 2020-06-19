28b8fc78339ebc18e9c3d578e03e9d040b12fe13
Lucas Gregory <lgregory12@hotmail.com>
2020-06-18 21:40:46 -0300

    Improve hash provider methods.

--- src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
+++ src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
@@ -1,14 +1,12 @@
 import IHashProvider from '../models/IHashProvider';
 
 class FakeHashProvider implements IHashProvider {
-  generateHash(payload: string): Promise<string> {
-    return new Promise(resolve => {
-      resolve(payload);
-    });
+  public async generateHash(payload: string): Promise<string> {
+    return payload;
   }
 
-  compareHash(payload: string, hashed: string): Promise<boolean> {
-    return new Promise(resolve => resolve(payload === hashed));
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
+    return payload === hashed;
   }
 }
 
