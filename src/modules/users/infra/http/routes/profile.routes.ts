43556e0e12867f1ce9c65be014f1bb533668351f
Lucas Gregory <lgregory12@hotmail.com>
2020-07-08 19:17:52 -0300

    feat: profile update.
    add get to profiles routes.
    change put to update profile route.

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -9,6 +9,7 @@ const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.post('/', profileController.update);
+profileRouter.put('/', profileController.update);
+profileRouter.get('/', profileController.show);
 
 export default profileRouter;
