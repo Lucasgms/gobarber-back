18b5af28edb5bef7256280df6b0e8f8f6ece6d92
Lucas Gregory <lgregory12@hotmail.com>
2020-07-10 19:18:00 -0300

    feat: create appointments.
    add find all appointments at month by provider in repositories.

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,8 +1,13 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthByProviderDTO from '../dtos/IFindAllInMonthByProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
 
   findByDate(date: Date): Promise<Appointment | undefined>;
+
+  findAllInMonthByProvider(
+    data: IFindAllInMonthByProviderDTO,
+  ): Promise<Appointment[]>;
 }
