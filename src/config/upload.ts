76dfa257913498c4b00e2ed8db4bdb55a9510171
Lucas Gregory <lgregory12@hotmail.com>
2020-07-20 19:18:49 -0300

    feat: production storage provider.
    add support to amazon s3 provider.

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,47 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 const uploadsFolder = path.resolve(__dirname, '..', '..', 'tmp', 'uploads');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tmpFolder,
   uploadsFolder,
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename: (request, file, callback) => {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename: (request, file, callback) => {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber',
     },
-  }),
-};
+  },
+} as IUploadConfig;
